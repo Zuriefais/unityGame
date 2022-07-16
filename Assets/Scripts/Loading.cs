053b4d476159fd8e72fdc566253dc1b4164dfbd9
Zuriefais <zuriefais@gmail.com>
2022-07-16 23:27:05 +0300

    loading

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public int sceneID;
+    public Slider loadSlider;
+
+    void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+    
+    IEnumerator LoadNextScene()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            loadSlider.value =  progress;
+            yield return null;
+        }
+    }
+}
