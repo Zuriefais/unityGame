25ba55fffc3315d7f55939b70679724db30c0560
Zuriefais <zuriefais@gmail.com>
2022-07-17 23:02:57 +0300

    PlayerController refactoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,8 +11,16 @@ public class PlayerController : NetworkBehaviour
     public GameObject mainCamera;
     private Vector2 direction;
     private CinemachineVirtualCamera cinemachineCamera;
-    // Start is called before the first frame update
-    void Start()
+
+    void cameraConnect()
+    {
+        if (isLocalPlayer)
+        {
+            Camera.cvc.Follow = gameObject.transform;
+        }
+    }
+
+    void positionUpdate()
     {
         SaveDate.PlayerData safeData = SaveMenager.Load<SaveDate.PlayerData>("playerSave.json");
         if (safeData.x == 0f)
@@ -21,29 +29,39 @@ public class PlayerController : NetworkBehaviour
             safeData.x = this.transform.position.x;
             safeData.y = this.transform.position.y;
 
-            SaveMenager.Save(safeData ,"PlayerSave.json");
+            SaveMenager.Save(safeData, "PlayerSave.json");
         }
         Debug.Log(SaveMenager.Load<SaveDate.PlayerData>("playerSave.json").playerName);
-        rb = GetComponent<Rigidbody2D>();
-        if (isLocalPlayer)
-        {
-            Camera.cvc.Follow = gameObject.transform;
-        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckInput()
     {
         if (isLocalPlayer)
         {
             direction.x = Input.GetAxisRaw("Horizontal");
             direction.y = Input.GetAxisRaw("Vertical");
         }
-        
+    }
+
+    void MovePosition()
+    {
+        rb.MovePosition(rb.position + direction.normalized * Time.fixedDeltaTime * speed);
+    }
+
+    void Start()
+    {
+        positionUpdate();
+        cameraConnect();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckInput();
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + direction.normalized * Time.fixedDeltaTime * speed);
+        MovePosition();
     }
 }
